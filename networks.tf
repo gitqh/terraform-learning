72fde4d7b7f22f7023b0f0fc7fb178f5f3b77db4
kobe19881208 <kobe19881208@163.com>
2018-06-07 16:24:43 +0800

    create terraform aws instance

--- networks.tf
+++ networks.tf
@@ -8,33 +8,6 @@ resource "aws_vpc" "our_development_network" {
   }
 }
 
-resource "aws_subnet" "subnet1" {
-  cidr_block        = "${cidrsubnet(aws_vpc.our_development_network.cidr_block, 3 ,1)}"
-  vpc_id            = "${aws_vpc.our_development_network.id}"
-  availability_zone = "us-west-2a"
-}
-
-resource "aws_subnet" "subnet2" {
-  # cidr_block = "${cidrsubnet(iprange, newbits, netnum)}"
-  cidr_block        = "${cidrsubnet(aws_vpc.our_development_network.cidr_block, 2 ,2)}"
-  vpc_id            = "${aws_vpc.our_development_network.id}"
-  availability_zone = "us-west-2b"
-}
-
-resource "aws_security_group" "subnetsecurity" {
-  vpc_id = "${aws_vpc.our_development_network.id}"
-
-  ingress = {
-    cidr_blocks = [
-      "${aws_vpc.our_development_network.cidr_block}",
-    ]
-
-    from_port = 80
-    to_port   = 80
-    protocol  = "tcp"
-  }
-}
-
 resource "azurerm_resource_group" "azy_network" {
   location = "West US"
   name     = "devresgrp"
