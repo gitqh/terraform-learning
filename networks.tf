b5a9617a42471fd6442015d41b601665eaa0fa44
kobe19881208 <kobe19881208@163.com>
2018-06-11 22:00:43 +0800

    add variables

--- networks.tf
+++ networks.tf
@@ -1,5 +1,5 @@
 resource "aws_vpc" "our_development_network" {
-  cidr_block           = "10.0.0.0/16"
+  cidr_block           = "${var.aws_ip_cidr_range}"
   enable_dns_hostnames = true
   enable_dns_support   = true
 
